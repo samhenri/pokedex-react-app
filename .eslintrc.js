9f331062a010eb9d3652815f9649aaad893c820e
Samuel Guimarães <samhenri@outlook.com>
2018-04-26 15:47:18 -0300

    Added some packages to dev

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+const paths = require('./config/paths');
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    commonjs: true,
+    node: true,
+    jest: true,
+    mocha: true,
+  },
+  extends: [
+    'wiremore',
+    'wiremore/react',
+    'plugin:react/recommended',
+    'plugin:flowtype/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  plugins: ['security', 'flowtype'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: paths.appNodeModules,
+      },
+    }
+  },
+  rules: {
+    indent: 0,
+    'import/named': 0,
+    'import/prefer-default-export': 'off',
+    'import/no-unassigned-import': 0,
+    'import/no-named-as-default-member': 0,
+  },
+};
