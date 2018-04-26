92e4bee3f4de85dd714ef6ee7df0333547a0b6af
Samuel Guimarães <samhenri@outlook.com>
2018-04-26 19:00:10 -0300

    Updated Babel version and added Styleguidist package and config

---
+++ styleguide.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  webpackConfig: require('./config/webpack.config.dev.js'),
+  title: 'Pokédex React App Styleguide',
+  defaultExample: true,
+  sections: [
+    { name: 'Introduction', content: './README.md' },
+    { name: 'Core UI', components: './src/core/components/**/[A-Z]*.js' },
+    { name: 'Pokémon View UI', components: './src/app/*/components/**/[A-Z]*.js' },
+  ],
+};
